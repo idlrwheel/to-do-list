c22e71a98bc13f22305217b896391569e4b1d278
giovanna ramos <161260067+idlrwheel@users.noreply.github.com>
2024-08-29 00:05:28 -0300

    Add files via upload

---
+++ src/components/ToDoList.js
@@ -0,0 +1,57 @@
+import React, {useState} from 'react'
+import '../index.css';
+
+function ToDoList(){
+    const [tasks, setTasks] = useState([]);
+    const [newTask, setNewTask] = useState("");
+
+    function handleInputChance(event){
+        setNewTask(event.target.value);
+    }
+
+    function addTask(){
+        if(newTask.trim() !== ""){
+            setTasks([...t, newTask, completed]);
+            setNewTask("");
+        }
+       
+    }
+    function deleteTask(index){
+        const updatedTasks = tasks.filter((_, i)=> i !== index);
+        setTasks(updatedTasks);
+    }
+
+    
+
+    function checkTask(index){
+
+    }
+
+
+    return(
+        <div className="to-do-list">
+            <h1>To-Do-List</h1>
+            <div>
+                <input type = "text" placeholder = "Digite uma tarefa..." value = {newTask} onChange = {handleInputChance}></input>
+                <button className = "add-button" onClick = {addTask}>
+                    Adicionar
+                </button>
+            </div>
+            <ol>
+                {tasks.map((task, index) =>
+                <li key = {index}>
+                    <span className="text">{task}</span>
+                    <button className="delete-button" onClick={() => deleteTask(index)}>
+                        Deletar
+                    </button>
+                    <button className="finish-button" onClick={() => checkTask(index)}>
+                        Concluída
+                    </button>
+                </li>
+                )}
+            </ol>
+        </div>
+    )
+}
+
+export default ToDoList
